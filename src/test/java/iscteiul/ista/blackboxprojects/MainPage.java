0ef38a6789469befc68db1afce1ec715d1c28dac
Junzun <jhenu@iscte-iul.pt>
2025-11-27 12:33:26 +0000

    Part1A3 LEI122687

--- src/test/java/iscteiul/ista/blackboxprojects/MainPage.java
+++ src/test/java/iscteiul/ista/blackboxprojects/MainPage.java
@@ -5,15 +5,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-// page_url = https://www.jetbrains.com/
 public class MainPage {
+
     @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
     public WebElement seeDeveloperToolsButton;
 
-    @FindBy(xpath = "//*[@data-test='suggestion-action']")
+    @FindBy(css = "a[data-test='suggestion-link']")
     public WebElement findYourToolsButton;
 
-    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
+    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker ='Developer Tools']")
     public WebElement toolsMenu;
 
     @FindBy(css = "[data-test='site-header-search-action']")
--- src/test/java/iscteiul/ista/blackboxprojects/MainPageTest.java
+++ src/test/java/iscteiul/ista/blackboxprojects/MainPageTest.java
@@ -1,17 +1,19 @@
 package iscteiul.ista.blackboxprojects;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class MainPageTest {
+
     private WebDriver driver;
     private MainPage mainPage;
 
@@ -19,8 +21,25 @@ public class MainPageTest {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
         driver.get("https://www.jetbrains.com/");
+
+        try {
+            List<WebElement> cookieContainers =
+                    driver.findElements(By.cssSelector("div.ch2-container"));
+            if (!cookieContainers.isEmpty()) {
+                WebElement container = cookieContainers.get(0);
+                List<WebElement> buttons = container.findElements(By.tagName("button"));
+                for (WebElement button : buttons) {
+                    if (button.isDisplayed() && button.isEnabled()) {
+                        button.click();
+                        System.out.println("Cookie banner accepted.");
+                        break;
+                    }
+                }
+            }
+        } catch (Exception ignored) {}
 
         mainPage = new MainPage(driver);
     }
@@ -31,33 +50,40 @@ public class MainPageTest {
     }
 
     @Test
-    public void search() {
+    public void search() throws InterruptedException {
+
         mainPage.searchButton.click();
 
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
+        WebElement searchField = driver.findElement(
+                                        By.cssSelector("input[data-test-id='search-input']"));
+
+        //searchField.clear();
         searchField.sendKeys("Selenium");
 
-        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
-        submitButton.click();
+        Thread.sleep(1000);
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
-        assertEquals("Selenium", searchPageField.getAttribute("value"));
+        assertEquals("Selenium", searchField.getAttribute("value"));
     }
 
     @Test
-    public void toolsMenu() {
+    public void toolsMenu() throws InterruptedException {
         mainPage.toolsMenu.click();
 
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
+        WebElement menuPopup = driver.findElement
+                                        (By.cssSelector("div[data-test='main-submenu-suggestion']"));
+
+        Thread.sleep(666);
         assertTrue(menuPopup.isDisplayed());
     }
 
     @Test
-    public void navigationToAllTools() {
+    public void navigationToAllTools() throws InterruptedException {
         mainPage.seeDeveloperToolsButton.click();
         mainPage.findYourToolsButton.click();
 
         WebElement productsList = driver.findElement(By.id("products-page"));
+
+        Thread.sleep(777);
         assertTrue(productsList.isDisplayed());
         assertEquals("All Developer Tools and Products by JetBrains", driver.getTitle());
     }
